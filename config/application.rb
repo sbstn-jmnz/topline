77ea68514ecf48047356996fe868bc25c090041e
Sofiaimg <sofia.img@gmail.com>
2018-04-11 19:07:57 -0300

    helpers

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,10 @@ require 'carrierwave/orm/activerecord'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module Topline
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
