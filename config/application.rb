c09b15df144b96c19c26f9f038216f14fd5e869b
Sofiaimg <sofia.img@gmail.com>
2018-04-11 16:23:58 -0300

    config/application file

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Topline
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -20,3 +20,4 @@ module Topline
 
   end
 end
+# config/application.rb
